33f64354e68f8a17a216f274ed822995e54d1240
RafaIzu <bomizukawa@gmail.com>
2020-07-12 20:02:48 -0300

    colocando as fontes corretas, adicionando estilo a pagina e sistema de colisoes

--- js/flappy.js
+++ js/flappy.js
@@ -123,6 +123,35 @@ setInterval(() =>{
 }, 20) 
  */
 
+
+function estaoSobrepostos(elementoA, elementoB) {
+    const a = elementoA.getBoundingClientRect()
+    const b = elementoB.getBoundingClientRect()
+
+    const horizontal = a.left + a.width >= b.left
+        && b.left + b.width >= a.left
+
+    const vertical = a.top + a.height >= b.top
+        && b.top + b.width >= a.top
+    
+    return horizontal && vertical
+    
+}
+
+function colidiu(passaro, barreiras) {
+    let colidiu = false
+    barreiras.pares.forEach(parDeBarreiras => {
+        if (!colidiu){
+            const superior = parDeBarreiras.superior.elemento
+            const inferior = parDeBarreiras.inferior.elemento
+            colidiu = estaoSobrepostos(passaro.elemento, superior)
+                || estaoSobrepostos(passaro.elemento, inferior)
+        }
+    })
+    return colidiu
+    
+}
+
 function FlappyBird(){
     let pontos = 0
 
@@ -144,6 +173,10 @@ function FlappyBird(){
         const temporizador = setInterval(() =>{
             passaro.animar()
             barreiras.animar()
+
+            if(colidiu(passaro, barreiras)){
+                clearInterval(temporizador)
+            }
         }, 20)
     }
 
